be1ea6c9ecd181f8b91dbc0f74cd1371ae1b8971
lishenguang1 <lishenguang1@126.com>
2019-11-29 15:56:44 +0800

    需要登录模块获取数据

--- src/components/video/VideoListenBGM.js
+++ src/components/video/VideoListenBGM.js
@@ -3,16 +3,12 @@ import {connect} from "react-redux";
 class VideoEvery extends React.Component{
     render(){
         return (
-            <div>
-
-            </div>
+            <div>sasasds</div>
         )
-    }
-    UNSAFE_componentWillMount() {
     }
 
     componentDidMount() {
-        this.props.getListenBgmList.call(this)
+ 
     }
 }
 function mapStateToProps(){
@@ -22,18 +18,7 @@ function mapStateToProps(){
 }
 function mapDispatchToProps(dispatch){
     return {
-            async getListenBgmList(){
-                dispatch(()=>{
-                    console.log(111111111)
-                    const {data} = this.$axios.get("/video/group",{
-                        params:{
-                            id:58101,
-                        }
-                    })
-                    console.log(data)
-                    console.log(222222222)
-                })
-            }
+
     }
 }
 export default connect(mapStateToProps,mapDispatchToProps)(VideoEvery)
