7b4fab9407b22914bbe637d7fc21e971079e83b0
jianli <15083493779@163.com>
2019-11-30 22:22:45 +0800

    11:30晚，完成了accountbottom部分

--- src/components/account/AccountCenter.js
+++ src/components/account/AccountCenter.js
@@ -1,10 +1,28 @@
-import React from "react";
-export default class AccountHeader extends React.Component{
+import React,{Fragment} from "react";
+import "../../assets/css/account/accountCenter.css";
+export default class AccountCenter extends React.Component{
     render(){
         return (
-            <div>
-                组件
-            </div>
+            <Fragment>
+                <div className={"cm_account_center_fragment"}>
+                    <div>
+                        <i className="iconfont icon-xiaoxixinfengnews"></i>
+                        <p>消息</p>
+                    </div>
+                    <div>
+                        <b className="iconfont icon-gouwuche"></b>
+                        <p>商城</p>
+                    </div>
+                    <div>
+                        <i className="iconfont icon-youhuiquan"></i>
+                        <p>演出</p>
+                    </div>
+                    <div>
+                        <b className="iconfont icon-shangyi"></b>
+                        <p>个性装扮</p>
+                    </div>
+                </div>
+            </Fragment>    
         )
     }
 }
